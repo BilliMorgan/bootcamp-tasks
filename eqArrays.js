edb2b8f5a5d8e4e00e35549a72497adcd09d09ea
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-08-11 13:20:57 -0400

    eqArrays is done

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = require('./assertEqual')
+
+const eqArrays = (arr1, arr2) => {
+  let comperison = false;
+  if (arr1.length === arr2.length) {
+    for (i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        comperison = true;
+      } else {
+        comperison = false;
+      }
+    }
+  }
+  return comperison;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
